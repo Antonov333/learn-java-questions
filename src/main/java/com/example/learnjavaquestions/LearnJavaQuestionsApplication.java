0ccf19da0844e6a3aad4690cdb0e6ad0230d9e75
Antonov333 <antonovsergei333@yahoo.com>
2024-03-02 20:19:43 +0300

    Initial commit

---
+++ src/main/java/com/example/learnjavaquestions/LearnJavaQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnjavaquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learnjavaquestions/LearnJavaQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnjavaquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
