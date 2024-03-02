8cb1de55649c409ddbc168f31f645a9a8aba92b0
Antonov333 <antonovsergei333@yahoo.com>
2024-03-02 22:05:07 +0300

    Minor changes...

--- src/main/java/com/example/learnjavaquestions/LearnJavaQuestionsHomeController.java
+++ src/main/java/com/example/learnjavaquestions/LearnJavaQuestionsHomeController.java
@@ -2,9 +2,7 @@ package com.example.learnjavaquestions;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
