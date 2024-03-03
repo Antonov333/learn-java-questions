225a8bb8734203287e7ef74443e00afd114aefd1
Antonov333 <antonovsergei333@yahoo.com>
2024-03-03 23:00:46 +0300

    Created endpoints to input and update questions

--- src/main/java/com/example/learnjavaquestions/InterviewQuestionService.java
+++ src/main/java/com/example/learnjavaquestions/InterviewQuestionService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -13,20 +15,34 @@ import java.util.Random;
 public class InterviewQuestionService {
 
     private final InterviewQuestionRepository questionRepository;
-    public ResponseEntity<InterviewQuestion> createQuestion(InterviewQuestion question) {
 
-        // null check
-        if(question == null) {
-            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(), HttpStatus.BAD_REQUEST);}
-        if (question.getQuestion() == null | question.getAnswer() == null){
-            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(), HttpStatus.BAD_REQUEST);}
+    public ResponseEntity<InterviewQuestion> createQuestion(@RequestBody InterviewQuestion question) {
 
-        //emptiness check
-        if(question.getQuestion().isEmpty() | question.getAnswer().isEmpty()) {
+        // Make sure interview question entity is correct, i.e. it is not null, question and answer fields are neither null, nor blank
+        // If not then return BAD REQUEST response code
+        if (!questionEntityIsCorrect(question)) {
             return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(), HttpStatus.BAD_REQUEST);
         }
 
-        return new ResponseEntity<InterviewQuestion>(questionRepository.save(question),HttpStatus.CREATED);
+        return new ResponseEntity<InterviewQuestion>(questionRepository.save(question), HttpStatus.CREATED);
+    }
+
+    /**
+     * <h3>boolean questionEntityIsCorrect(InterviewQuestion question)</h3>
+     * Make sure interview question entity is correct, i.e. it is not null, question and answer fields are neither null, nor blank
+     *
+     * @param question {@link InterviewQuestion} entity to be checked whether it is correct, i.e. neither null, nor blank
+     * @return true if supplied argument is correct
+     */
+    boolean questionEntityIsCorrect(InterviewQuestion question) {
+        if (question == null) {
+            return false;
+        }
+        if (question.getQuestion() == null | question.getAnswer() == null |
+                question.getTopic() == null) {
+            return false;
+        }
+        return !(question.getQuestion().isBlank() | question.getAnswer().isBlank());
     }
 
     public List<InterviewQuestion> learnAll() {
@@ -40,5 +56,59 @@ public class InterviewQuestionService {
         InterviewQuestion question = questionRepository.findById(questionNumber).orElse(null);
         return new ResponseEntity<InterviewQuestion>(question, HttpStatus.OK);
         // TODO: think how to better check and manage questions
+    }
+
+    public ResponseEntity<InterviewQuestion> learnQuestion(int questionId) {
+        // make sure id is correct. If not then return BAD REQUEST response code
+        if (!questionIdIsCorrect(questionId)) {
+            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(), HttpStatus.BAD_REQUEST);
+        }
+
+        // look up question specified by supplied id and make sure mentioned question exists
+        // If not then return NOT FOUND response code
+        Optional<InterviewQuestion> questionOptional = questionRepository.findById(questionId);
+        if (questionOptional.isEmpty()) {
+            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(), HttpStatus.NOT_FOUND);
+        }
+        // Deliver found question to requested side
+        return new ResponseEntity<>(questionOptional.get(), HttpStatus.OK);
+    }
+
+    boolean questionIdIsCorrect(int questionId) {
+        return (questionId > 0 & questionId <= questionRepository.count());
+    }
+
+    public ResponseEntity<InterviewQuestion> updateQuestion(int questionId,
+                                                            @RequestBody InterviewQuestion questionUpdates) {
+        // make sure supplied arguments are correct
+        if (!questionIdIsCorrect(questionId)) {
+            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(),
+                    HttpStatus.BAD_REQUEST);
+        }
+        // make sure questionUpdate neither null, nor empty. If not then return BAD REQUEST response code
+        if (!questionEntityIsCorrect(questionUpdates)) {
+            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // look up question specified by supplied id and make sure mentioned question exists
+        // If not then return NOT FOUND response code
+        Optional<InterviewQuestion> questionOptional = questionRepository.findById(questionId);
+        if (questionOptional.isEmpty()) {
+            return new ResponseEntity<InterviewQuestion>(new InterviewQuestion(),
+                    HttpStatus.NOT_FOUND);
+        }
+
+        // Update question entity
+        InterviewQuestion updatedQuestion = questionOptional.get();
+        updatedQuestion.setQuestion(questionUpdates.getQuestion());
+        updatedQuestion.setAnswer(questionUpdates.getAnswer());
+        updatedQuestion.setTopic(questionUpdates.getTopic());
+
+        // Save update question in database
+        updatedQuestion = questionRepository.save(updatedQuestion);
+
+        // Deliver updated question to requested side
+        return new ResponseEntity<InterviewQuestion>(updatedQuestion, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/learnjavaquestions/QuestionsController.java
+++ src/main/java/com/example/learnjavaquestions/QuestionsController.java
@@ -1,14 +1,12 @@
 package com.example.learnjavaquestions;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +35,28 @@ public class QuestionsController {
     @GetMapping("/random")
     public ResponseEntity<InterviewQuestion> randomQuestion() {
         return questionService.randomQuestion();
+    }
+
+    @GetMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Question successfully delivered"),
+            @ApiResponse(responseCode = "404", description = "No question specified by provided id"),
+            @ApiResponse(responseCode = "400", description = "Question id must be over zero and lesser or equal than total number of question stored in database. Then, questionUpdates must be not null and not empty")})
+    public ResponseEntity<InterviewQuestion> learnQuestion(@Parameter(name = "id", description = "Question database identifier")
+                                                           @PathVariable(name = "id")
+                                                           int questionId) {
+        return questionService.learnQuestion(questionId);
+    }
+
+    @PutMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Question successfully updated"),
+            @ApiResponse(responseCode = "404", description = "No question specified by provided id"),
+            @ApiResponse(responseCode = "400", description = "Question id must be over zero and lesser or equal than total number of question stored in database. Then, questionUpdates must be not null and not empty")}
+    )
+    public ResponseEntity<InterviewQuestion> updateQuestion(@Parameter(name = "id", description = "Question database identifier")
+                                                            @PathVariable(name = "id")
+                                                            int questionId, InterviewQuestion questionUpdates) {
+        return questionService.updateQuestion(questionId, questionUpdates);
     }
 }
